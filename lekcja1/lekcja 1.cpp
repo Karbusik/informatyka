2835f5d12f08f8bd1685667e1cc64f44388d3183
karbusik <karbo08@prokonto.pl>
2023-10-25 14:08:45 +0200

    komentarz

---
+++ lekcja1/lekcja 1.cpp
@@ -0,0 +1,22 @@
+ #include <iostream>
+ using namespace std;
+ 
+int main (){
+	
+	short liczba = 2;
+	float zmiennoprzecinkowa = 3.532;
+	string tekst = "tekst";
+	char znak = 'a';
+	bool znakowy = false;
+	cout << "Hello world" << endl;
+	cout << "liczba calkowita: " << liczba;
+	cout << endl;
+	
+	string jakos;
+	cout << "podaj jakis wyraz: ";
+	cin >>jakos;
+	cout << " Wprowadziles wartosc: " << jakos;
+	
+	 	
+	return 0;
+} 
